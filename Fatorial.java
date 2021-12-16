d763c762d144a0c0e7ec64f751783799658b68c8
Maurício dos Santos <mauriciodssantosds@gmail.com>
2021-12-16 07:52:40 -0300

    criação do arquivo principal da aplicação com método main

--- Fatorial.java
+++ Fatorial.java
@@ -1,9 +1,6 @@
 public class Fatorial {
-	public static void main(String [] args) {
-		System.out.print(calularFatorial(5));
-	}
 	
-	public static int calularFatorial(int n) {
+	public int calcularFatorial(int n) {
 		int resultado = 1;
 		
 		for (int i=2; i <=n; i++)
