1c3f01fa63875348e005fa482179254b8cc46fd5
Maurício dos Santos <mauriciodssantosds@gmail.com>
2022-09-11 10:32:43 -0300

    Comentários em Fatorial.java atualizados

--- Fatorial.java
+++ Fatorial.java
@@ -1,5 +1,5 @@
 public class Fatorial {
-	// versão iterativa do cálculo do fatorial
+	// versão iterativa do cálculo do fatorial, incluindo o teste para n=0
 	public int calcularFatorial(int n) throws IllegalArgumentException {
 		int resultado = 1;
 		if (n < 0) 
